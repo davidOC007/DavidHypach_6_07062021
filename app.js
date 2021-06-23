bb2df35b3a3a5ad3d196a4da188a4f8cf58db28b
David Hypach <d_hypach@hotmail.com>
2021-06-23 17:23:06 +0200

    Implémentation variable environnement

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const helmet = require ('helmet');
+const bodyParser = require('body-parser');
+const path = require('path');
+const limiter = require("./middleware/limiter");
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+require('dotenv').config();
+mongoose.connect(process.env.DB_URI,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-module.exports = app;
+  app.use(bodyParser.json());
+
+  app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Content-Type, Authorization');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+      next();
+  });
+  
+  app.use(helmet());
+  app.use("/api/auth", limiter);
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+  app.use('/api/auth', userRoutes);
+  app.use('/api/sauces', sauceRoutes);
+  
+  module.exports = app;
